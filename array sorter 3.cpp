d7495e6005042973b98c62973dada5b4e4e7763b
Adithyan S <83820438+adithyans369@users.noreply.github.com>
2021-05-07 19:05:51 +0530

    Add files via upload

---
+++ array sorter 3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,temp;
+    cout << "enter the number of elements of the array"<<endl;
+    cin >> n;
+    int a[n];
+    if((n<=0)||(n>10))
+       cout << "invalid array size";
+   else
+    {
+    cout << "enter the numbers to the array:" << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cout << "***********************************************"<<endl;
+    cout << "The unsortd array elements are: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "a"<< "[" << i << "]"<< "= " << a[i] << endl;
+    }
+    cout << "***********************************************"<<endl;
+    cout << "the sorted array elements are:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+       
+        cout<<"a"<< "[" << i << "]"<< "= " << a[i] << endl;
+    }
+    cout << "   \t   \t ****************************GOT THE ANSWER ? THEN GO AND FUCK UP:)****************************"<<endl;
+    }
+    return 0;
+}
